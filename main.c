6cd80f0f10dc48e0f577b99692953eb83e6fde9e
asdfghj12345795 <55428385+asdfghj12345795@users.noreply.github.com>
2019-10-08 15:30:49 +0900

    submit_05

--- main.c
+++ main.c
@@ -6,15 +6,9 @@
 int main(int argc, char *argv[]) 
 
 {
-	int i=0, meter;
-
-	
-	while(i<3)
-	{
-		meter=i*1609;
-		printf("%d mile is %d meter.\n", i, meter);
-		i++;
-	}
-	
+	int i;
+	for(i=0;i<10;i++)
+	 printf("Hello world.\n");
+	 
 	return 0;
 }
